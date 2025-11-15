23e5f335a1df413bdf6be661f76227b1d4baa7ea
Evan F <84680760+evenevan@users.noreply.github.com>
2025-11-15 15:53:16 -0800

    fix: increase body nax size

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const app = express();
 const port = process.env["PORT"] ?? 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
